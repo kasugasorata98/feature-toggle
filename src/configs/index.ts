0e4756ad7d09d84469c49063b05c4561b060cbed
kasugasorata98 <jeromykho98@gmail.com>
2023-03-16 02:00:39 +0800

    commit

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,25 +1,28 @@
-import development from './development'
-import production from './production'
-import staging from './staging'
-import dotenv from 'dotenv'
-dotenv.config()
+import development from "./development";
+import production from "./production";
+import staging from "./staging";
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "development") {
+  dotenv.config();
+}
 
 export const config = (() => {
-  switch (process.env.NODE_ENV || 'development') {
-    case 'prod':
-    case 'production': {
-      return production()
+  switch (process.env.NODE_ENV || "development") {
+    case "prod":
+    case "production": {
+      return production();
     }
-    case 'stag':
-    case 'staging': {
-      return staging()
+    case "stag":
+    case "staging": {
+      return staging();
     }
-    case 'dev':
-    case 'development': {
-      return development()
+    case "dev":
+    case "development": {
+      return development();
     }
     default: {
-      return development()
+      return development();
     }
   }
-})()
+})();
